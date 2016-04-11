0d9becb65cf39574deb848140aa560c6be6d41c6
Samuel Rundengan <rockman_vforce@hotmail.com>
2016-04-11 10:39:26 +0200

    extends rectangle onzin

--- src/gamepackage/Tile.java
+++ src/gamepackage/Tile.java
@@ -1,8 +1,10 @@
 package gamepackage;
 
+import java.awt.geom.Rectangle2D;
+
 import com.sun.javafx.geom.Rectangle;
 
-public class Tile {
+public class Tile extends Rectangle2D.Double {
 
 	String type;
 	Tile(String type) {
